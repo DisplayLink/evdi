dc3c9d6bd835a0ad4c242089ce4fce1f38368ce7
Lukasz Spintzyk <lukasz.spintzyk@displaylink.com>
2018-02-12 11:16:17 +0100

    Add initial_device_count evdi parameter

--- module/evdi_drv.c
+++ module/evdi_drv.c
@@ -118,6 +118,24 @@ static void evdi_add_device(void)
 	evdi_context.dev_count++;
 }
 
+static int evdi_add_devices(unsigned int val)
+{
+	if (val == 0) {
+		EVDI_WARN("Adding 0 devices has no effect\n");
+		return 0;
+	}
+	if (val > EVDI_DEVICE_COUNT_MAX - evdi_context.dev_count) {
+		EVDI_ERROR("Evdi device add failed. Too many devices.\n");
+		return -EINVAL;
+	}
+
+	EVDI_DEBUG("Increasing device count to %u\n",
+		   evdi_context.dev_count + val);
+	while (val--)
+		evdi_add_device();
+	return 0;
+}
+
 /*
  * In 4.12+, the DRM core moves from the load() callback to requiring drivers
  * to open-code their registration in their probe callback.
@@ -222,24 +240,16 @@ static ssize_t add_store(__always_unused struct device *dev,
 			 const char *buf, size_t count)
 {
 	unsigned int val;
+	int ret;
 
 	if (kstrtouint(buf, 10, &val)) {
 		EVDI_ERROR("Invalid device count \"%s\"\n", buf);
 		return -EINVAL;
 	}
-	if (val == 0) {
-		EVDI_WARN("Adding 0 devices has no effect\n");
-		return count;
-	}
-	if (val > EVDI_DEVICE_COUNT_MAX - evdi_context.dev_count) {
-		EVDI_ERROR("Evdi device add failed. Too many devices.\n");
-		return -EINVAL;
-	}
 
-	EVDI_DEBUG("Increasing device count to %u\n",
-		   evdi_context.dev_count + val);
-	while (val--)
-		evdi_add_device();
+	ret = evdi_add_devices(val);
+	if (ret)
+		return ret;
 
 	return count;
 }
@@ -291,7 +301,7 @@ static struct device_attribute evdi_device_attributes[] = {
 
 static int __init evdi_init(void)
 {
-	int i;
+	int i, ret;
 
 	EVDI_INFO("Initialising logging on level %u\n", evdi_loglevel);
 
@@ -309,7 +319,14 @@ static int __init evdi_init(void)
 					   &evdi_device_attributes[i]);
 		}
 
-	return platform_driver_register(&evdi_platform_driver);
+	ret = platform_driver_register(&evdi_platform_driver);
+	if (ret)
+		return ret;
+
+	if (evdi_initial_device_count)
+		return evdi_add_devices(evdi_initial_device_count);
+
+	return 0;
 }
 
 static void __exit evdi_exit(void)
