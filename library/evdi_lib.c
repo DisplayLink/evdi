7aa55961d5b2924a517f2a439887542cede10512
mrTsjolder <mrtsjolder@gmail.com>
2018-04-20 13:07:13 +0200

    Conform to strict rules of SPDX licence tags 2

--- library/evdi_lib.c
+++ library/evdi_lib.c
@@ -1,6 +1,5 @@
-/* SPDX-License-Identifier: LGPL-2.1-only
- * Copyright (c) 2015 - 2017 DisplayLink (UK) Ltd.
- */
+// SPDX-License-Identifier: LGPL-2.1-only
+// Copyright (c) 2015 - 2017 DisplayLink (UK) Ltd.
 #include <stddef.h>
 #include <stdint.h>
 #include <libdrm/drm.h>
