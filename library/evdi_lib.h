fd0920601228d95d1548ad97f9fe91cec9b53000
Dawid Kurek <39980971+displaylink-dkurek@users.noreply.github.com>
2019-02-20 08:41:54 +0100

    Revert "Fix checkpatch.py warnings and errors in library"
    This reverts commit 40a79bb1d5077c467c9ab1322defacfa10acca44.

--- library/evdi_lib.h
+++ library/evdi_lib.h
@@ -13,61 +13,59 @@ struct evdi_device_context;
 typedef struct evdi_device_context *evdi_handle;
 typedef int evdi_selectable;
 
-enum evdi_device_status {
-	AVAILABLE,
-	UNRECOGNIZED,
-	NOT_PRESENT
-};
+typedef enum {
+  AVAILABLE,
+  UNRECOGNIZED,
+  NOT_PRESENT
+} evdi_device_status;
 
-struct evdi_rect {
-	int x1, y1, x2, y2;
-};
+typedef struct {
+  int x1, y1, x2, y2;
+} evdi_rect;
 
-struct evdi_mode {
-	int width;
-	int height;
-	int refresh_rate;
-	int bits_per_pixel;
-	unsigned int pixel_format;
-};
+typedef struct {
+  int width;
+  int height;
+  int refresh_rate;
+  int bits_per_pixel;
+  unsigned int pixel_format;
+} evdi_mode;
 
-struct evdi_buffer {
-	int id;
-	void *buffer;
-	int width;
-	int height;
-	int stride;
+typedef struct {
+    int id;
+    void* buffer;
+    int width;
+    int height;
+    int stride;
 
-	struct evdi_rect *rects;
-	int rect_count;
-};
+    evdi_rect* rects;
+    int rect_count;
+} evdi_buffer;
 
-struct evdi_event_context {
-	void (*dpms_handler)(int dpms_mode, void *user_data);
-	void (*mode_changed_handler)(struct evdi_mode mode, void *user_data);
-	void (*update_ready_handler)(int buffer_to_be_updated, void *user_data);
-	void (*crtc_state_handler)(int state, void *user_data);
-	void *user_data;
-};
+typedef struct {
+  void (*dpms_handler)(int dpms_mode, void* user_data);
+  void (*mode_changed_handler)(evdi_mode mode, void* user_data);
+  void (*update_ready_handler)(int buffer_to_be_updated, void* user_data);
+  void (*crtc_state_handler)(int state, void* user_data);
+  void* user_data;
+} evdi_event_context;
 
 #define EVDI_INVALID_HANDLE NULL
 
-enum evdi_device_status evdi_check_device(int device);
+evdi_device_status evdi_check_device(int device);
 evdi_handle evdi_open(int device);
-int evdi_add_device(void);
+int evdi_add_device();
 void evdi_close(evdi_handle handle);
-void evdi_connect(evdi_handle handle, const unsigned char *edid,
-		  const unsigned int edid_length,
+void evdi_connect(evdi_handle handle, const unsigned char* edid,
+		  const unsigned edid_length,
 		  const uint32_t sku_area_limit);
 void evdi_disconnect(evdi_handle handle);
-void evdi_grab_pixels(evdi_handle handle,
-		      struct evdi_rect *rects,
-		      int *num_rects);
-void evdi_register_buffer(evdi_handle handle, struct evdi_buffer buffer);
+void evdi_grab_pixels(evdi_handle handle, evdi_rect *rects, int *num_rects);
+void evdi_register_buffer(evdi_handle handle, evdi_buffer buffer);
 void evdi_unregister_buffer(evdi_handle handle, int bufferId);
 bool evdi_request_update(evdi_handle handle, int bufferId);
 
-void evdi_handle_events(evdi_handle handle, struct evdi_event_context *evtctx);
+void evdi_handle_events(evdi_handle handle, evdi_event_context* evtctx);
 evdi_selectable evdi_get_event_ready(evdi_handle handle);
 
 #ifdef __cplusplus
