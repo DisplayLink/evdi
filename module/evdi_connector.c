46938d954de43ff420f647dbfd8e499fab649d73
Dawid Kurek <dawid.kurek@displaylink.com>
2020-02-17 13:22:58 +0100

    Return number of modes added from evdi_get_modes
    Fixes #177

--- module/evdi_connector.c
+++ module/evdi_connector.c
@@ -51,7 +51,7 @@ static int evdi_get_modes(struct drm_connector *connector)
 #endif
 
 	if (!ret)
-		drm_add_edid_modes(connector, edid);
+		ret = drm_add_edid_modes(connector, edid);
 	else
 		EVDI_ERROR("Failed to set edid modes! error: %d", ret);
 
