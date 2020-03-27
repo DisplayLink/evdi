258147ea60e7f2953c1d0138ce977ac35a2b7347
Dawid Kurek <dawid.kurek@displaylink.com>
2020-03-27 09:53:18 +0100

    Add script to set version for both library and module

--- library/evdi_lib.h
+++ library/evdi_lib.h
@@ -13,7 +13,7 @@ extern "C" {
 
 #define LIBEVDI_VERSION_MAJOR 1
 #define LIBEVDI_VERSION_MINOR 6
-#define LIBEVDI_VERSION_PATCH 1
+#define LIBEVDI_VERSION_PATCH 4
 
 struct evdi_lib_version {
 	int version_major;
--- module/evdi_drv.h
+++ module/evdi_drv.h
@@ -38,11 +38,11 @@
 
 #define DRIVER_NAME   "evdi"
 #define DRIVER_DESC   "Extensible Virtual Display Interface"
-#define DRIVER_DATE   "20180913"
+#define DRIVER_DATE   "20200327"
 
 #define DRIVER_MAJOR 1
 #define DRIVER_MINOR 6
-#define DRIVER_PATCH 1
+#define DRIVER_PATCH 4
 
 struct evdi_fbdev;
 struct evdi_painter;
