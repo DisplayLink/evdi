fa7ce1ecdb67df58b0a9aec2e3bcf11efd734c03
Katarzyna Kępińska <katarzyna.kepinska@synaptics.com>
2022-07-20 14:37:45 +0200

    Add check_device and add_device functions to PyEvdi

--- pyevdi/PyEvdi.cpp
+++ pyevdi/PyEvdi.cpp
@@ -9,6 +9,17 @@ PYBIND11_MODULE(PyEvdi, m) {
 
     evdi_lib_version lv;
     evdi_get_lib_version(&lv);
-    
-    m.attr("version") = py::make_tuple(lv.version_major, lv.version_minor, lv.version_patchlevel);
+
+    m.attr("version") = py::make_tuple(lv.version_major, lv.version_minor, lv.version_patchlevel); 
+
+    m.def("check_device", &evdi_check_device);
+
+    m.def("add_device", &evdi_add_device);
+
+    py::enum_<evdi_device_status>(m, "evdi_device_status")
+        .value("AVAILABLE", AVAILABLE)
+        .value("UNRECOGNIZED", UNRECOGNIZED)
+        .value("NOT_PRESENT", NOT_PRESENT)
+        .export_values(); 
+
 }
---
+++ pyevdi/test/test_check_device.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2022 DisplayLink (UK) Ltd.
+import PyEvdi
+import pytest
+import os
+import re
+
+def is_not_running_as_root():
+    return os.geteuid() != 0
+
+def testCheckDevice():
+    for i in get_available_devices():
+         assert PyEvdi.check_device(i) == PyEvdi.AVAILABLE
+    assert PyEvdi.check_device(0) == PyEvdi.UNRECOGNIZED
+    assert PyEvdi.check_device(25) == PyEvdi.NOT_PRESENT
+
+@pytest.mark.skipif(is_not_running_as_root(), reason = 'Only root can add a new device.')
+def testAddDevice():
+    assert PyEvdi.add_device() == 1
+
+def get_available_devices():
+    list_of_available_devices = []
+    list_evdi = os.listdir('/sys/devices/platform/')
+    r = re.compile("evdi*")
+    list_evdi = list(filter(r.match, list_evdi))
+
+    for s in list_evdi:
+        list2 = os.listdir('/sys/devices/platform/' + s + '/drm')
+        r = re.compile("card*")
+        list_of_available_devices += list(filter(r.match, list2))
+
+    list_of_available_devices = [int(i[4:]) for i in list_of_available_devices]
+    return list_of_available_devices
