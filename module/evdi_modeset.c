2dd909cf33cdc016c7987b85fcd14dd64727f20f
Michael L. Young <elgueromexicano@gmail.com>
2022-07-21 21:30:45 -0400

    Fix Compiling On EL 8 Systems
    There are backports from newer kernels that have been put in the
    older kernels support by EL 8 based systems.  This patch fixes
    compiling on such systems.

--- module/evdi_modeset.c
+++ module/evdi_modeset.c
@@ -538,7 +538,7 @@ void evdi_modeset_init(struct drm_device *dev)
 
 void evdi_modeset_cleanup(__maybe_unused struct drm_device *dev)
 {
-#if KERNEL_VERSION(5, 8, 0) <= LINUX_VERSION_CODE
+#if KERNEL_VERSION(5, 8, 0) <= LINUX_VERSION_CODE || defined(EL8)
 #else
 	drm_mode_config_cleanup(dev);
 #endif
