c9c92c4a3c051b75ae924206fd9ad6172ae007a3
SimPilotAdamT <adam_tazul@outlook.com>
2022-10-08 18:02:02 +0100

    drm_framebuffer is only included on 6.0.0 and later

--- module/evdi_drm_drv.h
+++ module/evdi_drm_drv.h
@@ -28,10 +28,9 @@
 #if KERNEL_VERSION(5, 15, 0) <= LINUX_VERSION_CODE || defined(EL9)
 #include <drm/drm_legacy.h>
 #else
-#if LINUX_VERSION(6, 0, 0) >= LINUX_VERSION_CODE
-#include <drm/drm_framebuffer.h>
-#else
 #include <drm/drm_irq.h>
+#if KERNEL_VERSION(6, 0, 0) <= LINUX_VERSION_CODE
+#include <drm/drm_framebuffer.h>
 #endif
 #endif
 #include <drm/drm_crtc.h>
