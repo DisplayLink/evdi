bf97bc9e3e5818c6e4e6c3c7754d70ac9afb72b7
Łukasz Spintzyk <lukasz.spintzyk@synaptics.com>
2026-01-20 14:46:20 +0100

    Set version to 1.14.13

--- library/evdi_lib.h
+++ library/evdi_lib.h
@@ -15,7 +15,7 @@ extern "C" {
 
 #define LIBEVDI_VERSION_MAJOR 1
 #define LIBEVDI_VERSION_MINOR 14
-#define LIBEVDI_VERSION_PATCH 12
+#define LIBEVDI_VERSION_PATCH 13
 
 struct evdi_lib_version {
 	int version_major;
