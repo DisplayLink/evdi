750044ce73cf2eec9178889c4f851f0ff01368b3
Łukasz Spintzyk <lukasz.spintzyk@synaptics.com>
2026-02-09 09:40:17 +0100

    Set version to 1.14.14

--- module/dkms_install.sh
+++ module/dkms_install.sh
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 # Copyright (c) 2023 DisplayLink (UK) Ltd.
 
-evdi_version='1.14.13'
+evdi_version='1.14.14'
 
 EVDI_DIR=$(dirname "${BASH_SOURCE[0]}")
 EVDI_REBOOT_RATIONALE=
