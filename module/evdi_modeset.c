DisplayLink/evdi

commit 85279a1dd0c103c6ea0532f764c3d4e55742f7e5

Changed flag variable names and added a null check

module/evdi_modeset.c
@@ -370,16 +370,18 @@ static void evdi_cursor_atomic_update(struct drm_plane *plane,
 
 		mutex_unlock(&plane->dev->struct_mutex);
 		if (!evdi->cursor_events_enabled) {
-                        if (efb->obj->on_wayland) {
-                            evdi_cursor_atomic_get_rect(&old_rect, old_state);
-                            evdi_cursor_atomic_get_rect(&rect, state);
+                        if (fb != NULL) {
+                            if (efb->obj->allow_sw_cursor_rect_updates) {
+                                evdi_cursor_atomic_get_rect(&old_rect, old_state);
+                                evdi_cursor_atomic_get_rect(&rect, state);
 
-			    evdi_painter_mark_dirty(evdi, &old_rect);
+                                evdi_painter_mark_dirty(evdi, &old_rect);
+                            }
+                            else {
+                                rect = evdi_painter_framebuffer_size(evdi->painter);
+                            }
+                            evdi_painter_mark_dirty(evdi, &rect);
                         }
-                        else {
-                            rect = evdi_painter_framebuffer_size(evdi->painter);
-                        }
-			evdi_painter_mark_dirty(evdi, &rect);
 			return;
 		}
 

module/evdi_drm_drv.h
@@ -77,7 +77,7 @@ struct evdi_gem_object {
 	struct reservation_object *resv;
 	struct reservation_object _resv;
 #endif
-        bool on_wayland;
+        bool allow_sw_cursor_rect_updates;
 };
 
 #define to_evdi_bo(x) container_of(x, struct evdi_gem_object, base)

module/evdi_gem.c
@@ -100,7 +100,7 @@ struct evdi_gem_object *evdi_gem_alloc_object(struct drm_device *dev,
 	obj->base.funcs = &gem_obj_funcs;
 #endif
 
-        obj->on_wayland = false;
+        obj->allow_sw_cursor_rect_updates = false;
 
 	mutex_init(&obj->pages_lock);
 
@@ -132,7 +132,7 @@ evdi_gem_create(struct drm_file *file,
 #else
 	drm_gem_object_put_unlocked(&obj->base);
 #endif
-        obj->on_wayland = evdi_was_called_by_mutter();
+        obj->allow_sw_cursor_rect_updates = evdi_was_called_by_mutter();
 	*handle_p = handle;
 	return 0;
 }
@@ -399,12 +399,12 @@ evdi_prime_import_sg_table(struct drm_device *dev,
 {
 	struct evdi_gem_object *obj;
 	int npages;
-        bool on_wayland;
+        bool called_by_mutter;
 
-        on_wayland = evdi_was_called_by_mutter();
+        called_by_mutter = evdi_was_called_by_mutter();
 
 	if (evdi_disable_texture_import ||
-            (on_wayland && strcmp(attach->dmabuf->owner->name, "amdgpu") == 0)) {
+            (called_by_mutter && strcmp(attach->dmabuf->owner->name, "amdgpu") == 0)) {
                 return ERR_PTR(-ENOMEM);
         }
 
@@ -426,7 +426,7 @@ evdi_prime_import_sg_table(struct drm_device *dev,
 	drm_prime_sg_to_page_addr_arrays(sg, obj->pages, NULL, npages);
 #endif
 	obj->sg = sg;
-        obj->on_wayland = on_wayland;
+        obj->allow_sw_cursor_rect_updates = called_by_mutter;
 	return &obj->base;
 }
 

commit 49ba0c2e3bb1fd38337b2ac0fc59e19eb3704c81

modeset: Dirty the last cursor sprite on unassign
Unassign takes fb == NULL, so the software path never dirtied the
old sprite and DisplayLink kept the last grab.

Closes: #588
Fixes: 85279a1 ("Changed flag variable names and added a null check")

module/evdi_modeset.c
@@ -385,6 +385,9 @@ static void evdi_cursor_atomic_update(struct drm_plane *plane,
 					rect = evdi_painter_framebuffer_size(evdi->painter);
 				}
 				evdi_painter_mark_dirty(evdi, &rect);
+			} else if (old_state && old_state->fb) {
+				evdi_cursor_atomic_get_rect(&old_rect, old_state);
+				evdi_painter_mark_dirty(evdi, &old_rect);
 			}
 			return;
 		}